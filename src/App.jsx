a5279550765bba657f09d16aa9fb39dc7898e973
guuseh <guus-99@live.nl>
2025-05-28 18:08:43 +0300

    kms

--- src/App.jsx
+++ src/App.jsx
@@ -51,7 +51,7 @@ function App() {
   const isNotSmall = useIsSmall();
 
   return (
-    <HashRouter basename={import.meta.env.BASE_URL}>
+    <HashRouter basename="/cftm">
 
     <Menu small={!isNotSmall} />
     <Header small={!isNotSmall} />
