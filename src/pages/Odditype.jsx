04c04e9ff5be82d121290d51f373d187c5ffbc4a
guuseh <guus-99@live.nl>
2025-05-29 22:19:26 +0300

    check font variant emoji thing

--- src/pages/Odditype.jsx
+++ src/pages/Odditype.jsx
@@ -1,12 +1,22 @@
-import React from 'react'
+import {useEffect} from 'react'
 
 const Odditype = () => {
+
+  useEffect(() => {
+        window.scrollTo({
+            top: 0,
+            left: 0,
+            behavior: "smooth"
+        });
+        document.title = "Odditype :) Guus Hoeberechts"
+  }, [])
+
   return (
     <div>
       <h5>typography app – 2023</h5>
         <h1>odditype</h1>
 
-        <p className="description"><a href="https://www.odditype.nl" target="_blank" rel="noreferrer">Odditype&#10548;&#xFE0E;</a> is a website that randomises a given typeface to create a completely new feeling for it. Depending on the typeface, the changes can be small or drastic.
+        <p className="description"><a href="https://www.odditype.nl" target="_blank" rel="noreferrer">Odditype&#10548;</a> is a website that randomises a given typeface to create a completely new feeling for it. Depending on the typeface, the changes can be small or drastic.
         The user can control the randomisation with the slider, upload their own fonts for changing, and download the final typeface as a useable font file.
         For an exhibition, a functionality was added to write your own message and print it out on a receipt printer. On the right is a selection of prints from Media Lab 30 Years Demo Day.</p>
 
